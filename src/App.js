ed4a0414c339c93da8c6d9d01ffd8c1d8a37a7bc
sharanabasappaGit <sharanudhammur001@gmail.com>
2024-01-31 20:41:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,78 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const cb = () => {
+    console.log(name)
+  }
+
+  // const callback = (cb) => {
+  //   return Promise.resolve()
+  // }
+
+  // console.log(callback(cb).then)
+  const name = "hello"
+
+  const callBack = new Promise((resolve, reject) => {
+    // resolve("data")
+    // reject("error")
+  })
+  callBack.then((value) => {
+    console.log(value)
+    return "allgood"
+  }).then((value) => {
+    console.log(value)
+  }).catch(() => {
+    console.log("error")
+  })
+
+  const adapter = () => {
+    const url = "http://localhost:3000"
+    const response = apiCall(url)
+    response.then((res) => {
+      console.log(res)
+    })
+  }
+
+  const apiCall = (url) => {
+    return new Promise((resolve, reject) => {
+      if (true) {
+        resolve("data")
+      } else {
+        reject("error")
+      }
+    })
+  }
+
+  adapter()
+
+  apiCall().then((val) => console.log(val))
+
+  const apiList = [apiCall(), apiCall(), apiCall(), apiCall()]
+
+  Promise.all(apiList).then((val) => console.log(val))
+
+  console.log("hello")
+
+  // const makeApicall = ()=>{
+  //   return Promise(())
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
