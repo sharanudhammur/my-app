6ada36efdbe1a240ee2bd8080e8a437e8d4f7e41
sharanabasappaGit <sharanudhammur001@gmail.com>
2024-03-04 06:54:25 +0530

    sasa

--- src/App.js
+++ src/App.js
@@ -1,91 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
-import { useEffect } from 'react';
-import { useLayoutEffect, PureComponent } from 'react';
+import { ChakraProvider } from '@chakra-ui/react'
+import Login from './Screens/Login';
 
 function App() {
 
-  const cb = () => {
-    console.log(name)
-  }
-
-  // const callback = (cb) => {
-  //   return Promise.resolve()
-  // }
-
-  // console.log(callback(cb).then)
-  const name = "hello111"
-
-  const callBack = new Promise((resolve, reject) => {
-    // resolve("data")
-    // reject("error")
-  })
-  callBack.then((value) => {
-    console.log(value)
-    return "allgood"
-  }).then((value) => {
-    console.log(value)
-  }).catch(() => {
-    console.log("error")
-  })
-
-  const adapter = () => {
-    const url = "http://localhost:3000"
-    const response = apiCall(url)
-    response.then((res) => {
-      console.log(res)
-      console.log(res)
-      console.log(res)
-      console.log(res)
-    })
-  }
-
-  const apiCall = (url) => {
-    return new Promise((resolve, reject) => {
-      if (true) {
-        resolve("data")
-      } else {
-        reject("error")
-      }
-    })
-  }
-
-  console.log("test")
-
-  adapter()
-
-  apiCall().then((val) => console.log(val))
-
-  const apiList = [apiCall(), apiCall(), apiCall(), apiCall()]
-
-  Promise.all(apiList).then((val) => console.log(val))
-
-  // const makeApicall = ()=>{
-  //   return Promise(())
-  // }
-
-  useLayoutEffect(() => {
-    console.log("2")
-  }, [])
-
-  console.log("1")
-
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route
+                index
+                element={
+                  // <ProtectedRoute>
+                    <Login />
+                  // </ProtectedRoute>
+                }
+              />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Login />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 }
